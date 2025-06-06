097ba13b19fa9fbba96dd50354017a0abaae5442
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-06-06 10:55:44 -0700

    solve some logical mistake

--- home.js
+++ home.js
@@ -2,31 +2,50 @@ let form = document.querySelector("#form");
 let id = document.querySelector("#id");
 let div = document.querySelector(".container");
 let uemail = JSON.parse(localStorage.getItem("useremail"));
-console.log(uemail)
-// console.log(allid)
+let username = document.querySelector(".username");
+let Logout = document.querySelector("#Logout");
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    let userid = id.value.trim();
-    let  filtertodo = usertodo.filter((tod) => tod.id === Number(userid));
-    let singleid = usertodo.find(t => t.id === Number(userid));
-    let singleId = uemail.find(t => t.id === Number(userid));
-    //  console.log(singleid.id)
-    //  console.log(filtertodo[0]?.userId);
+    let userid = Number(id.value.trim());
+    //step 1one check usertodo userid
+    let filtertodo = usertodo.filter((tod) => tod.userId === userid);
+    //tep 2two   in obj  
+    let userExists = uemail.find(u => u.id === userid);
+    // let singleId = uemail.find(t => t.id === Number(userid));
 
+    let i = 1;
 
-     let i=1;
+    if (userExists && filtertodo) {
 
-        if (singleid.userId === Number(userid) && singleId.id ===singleid.userId ) {
+        let todos = filtertodo.map(t => {
+            return `<p> ${[i++]}   ,      ${t.title}  ,    ( ${t.completed} )</P>`
+        }).join("   ");
+        div.innerHTML = `${todos}`;
+       username.innerHTML = `UserName:  ${userExists.name}`;
 
-            let todos = filtertodo.map(t =>{ 
-                return `<p> ${[i++]}   ,      ${t.title}  ,    ( ${t.completed} )</P>`
-            }).join("   ");
-
-           div.innerHTML=`${todos}`;
-
+    }
+    else {
+        alert("Please enter your vailed ID");
+    };
+    Logout.addEventListener("click", () => {
+        // find index of user who logged in
+        let index = uemail.findIndex(u => u.id === userid);
+    
+        if (index !== -1) {
+            // remove the user from array
+            uemail.splice(index, 1);
+    
+            // update localStorage
+            localStorage.setItem("useremail", JSON.stringify(uemail));
+    
+            window.location.href = "index.html";
+        } else {
+            alert("کچھ مسئلہ ہے یار!");
         }
-        else {
-            alert("Please enter your vailed ID");
-        };
-});
+    });
+    });
+    
+
+//Logputing of user
